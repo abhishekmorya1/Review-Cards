522c0b101796aff7be842219ad660e0634681260
abhishekmorya1 <abhimorya8826@gmail.com>
2024-06-30 00:09:05 +0530

    my first commit

---
+++ src/data.js
@@ -0,0 +1,80 @@
+import Pallavi from './components/images/pallavi.png'
+import kshama from './components/images/kshama.png'
+import krrish from './components/images/krrish.png'
+import mitali from './components/images/mitali.jpg'
+import abhishek from './components/images/abhishek.jpg'
+import karan from './components/images/karan.jpg'
+import komal from './components/images/komal.png'
+import kunal from './components/images/kunal.png'
+import parul from './components/images/parul.png'
+
+const reviews = [
+  {
+    id: 1,
+    name: "Abhishek Morya",
+    job: "Software Engineer",
+    image: abhishek,
+    text: "As a Software Engineer, I have had the pleasure of working with this team on several projects. Their technical expertise and ability to deliver high-quality solutions on time and within budget consistently impress me. They are a true partner and an invaluable asset to any project. I highly recommend them for their professionalism and dedication."
+  },
+  {
+    id: 2,
+    name: "Karan Sharma",
+    job: "Software Engineer",
+    image: karan,
+    text: "As a Software Engineer, I have had the pleasure of working with this company for several years. Their technical expertise and commitment to innovation consistently impress me. They approach each project with creativity and meticulous attention to detail, ensuring exceptional results every time. Their professionalism and dedication to excellence truly set them apart. I highly recommend their services."
+  },
+  {
+    id: 3,
+    name: "Krrish Gulia",
+    job: "Associate Software Engineer",
+    image: krrish,
+    text: "As an Associate Software Engineer, I had the opportunity to work with this talented team on a software development project. Their technical expertise and collaborative approach were instrumental in overcoming complex challenges. The team's dedication to delivering high-quality software solutions was evident throughout the project. I am grateful for their exceptional work and highly recommend them!"
+  },
+  {
+    id: 4,
+    name: "Komal",
+    job: "Android Developer",
+    image: komal,
+    text: "As an Android Developer, I had the pleasure of working with this team on a mobile application project. Their expertise in Android development and their innovative approach to problem-solving were truly impressive. They consistently delivered high-quality code and ensured a seamless user experience. I highly recommend them for any mobile development needs!"
+  },
+  {
+    id: 5,
+    name: "Kshama",
+    job: "Android Developer",
+    image: kshama,
+    text: "As an Android Developer, I had the opportunity to collaborate with this team on a mobile application project. I was impressed with their proficiency in Android development and their ability to tackle complex coding challenges. Their innovative approach and commitment to delivering a seamless user experience were key factors in the project's success. It was a pleasure working with such a talented and dedicated team."
+  },
+  {
+    id: 6,
+    name: "Pallavi Gautam",
+    job: "Java Developer",
+    image: Pallavi,
+    text: "As a Java Developer, I had the opportunity to collaborate with this team on a software development project. I was impressed with their robust coding standards and their ability to solve complex programming challenges. Their expertise in Java and their dedication to delivering high-quality code significantly contributed to the project's success. It was a pleasure working with such a skilled and detail-oriented team."
+  },
+  {
+    id: 6,
+    name: "Mitali Munjal",
+    job: "Data Analyst",
+    image: mitali,
+    text: "As a Data Analyst, I had the opportunity to delve into complex datasets and extract valuable insights. The team's collaborative approach and attention to detail greatly enhanced our ability to make informed business decisions. Their expertise in handling data and providing actionable insights was instrumental in driving our project's success. It was a pleasure working with such a proficient and dedicated team."
+  },
+  {
+    "id": 6,
+    "name": "Parul Bhardwaj",
+    "job": "Web Developer",
+    "image": parul,
+    "text": "As a Web Developer, I had the opportunity to collaborate with this team on a web development project. I was impressed with their adherence to best practices in front-end and back-end development, as well as their ability to tackle intricate technical challenges. Their expertise in web technologies and commitment to delivering polished solutions significantly contributed to the project's success. It was a pleasure working with such a skilled and detail-oriented team."
+},
+{
+  "id": 6,
+  "name": "Kunal Dixit",
+  "job": "Java Developer",
+  "image": kunal,
+  "text": "As a Java Developer and team leader, I had the opportunity to lead our team on numerous software projects. I was impressed by their adherence to rigorous coding standards and their ability to effectively address complex programming challenges. My deep expertise in Java programming, combined with our team's dedication to delivering robust, scalable solutions, played a pivotal role in achieving success across multiple projects. It was truly rewarding to collaborate with such a talented and detail-oriented team."
+}
+
+
+  
+];
+
+export default reviews;
